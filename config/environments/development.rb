8ba5b9a85e231d1cad94b9547cce22e36cb85b5e
Joe Christensen <joechristensenwork@gmail.com>
2021-10-05 12:15:00 -0600

    Fixed Exposed Keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: "budgeteersavings@gmail.com",
-    password: "TestingEmail1234",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true,
   }
